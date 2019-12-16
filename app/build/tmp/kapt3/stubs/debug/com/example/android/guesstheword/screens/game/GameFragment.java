af5b2f5173e4195e72237cdc9385f136b8cbebdc
mingxu <taymx-jm17@student.tarc.edu.my>
2019-12-16 21:05:20 +0800

    Initial commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/guesstheword/screens/game/GameFragment.java
@@ -0,0 +1,44 @@
+package com.example.android.guesstheword.screens.game;
+
+import java.lang.System;
+
+/**
+ * Fragment where the game is played
+ */
+@kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u00006\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\b\u001a\u00020\tH\u0002J\b\u0010\n\u001a\u00020\tH\u0002J&\u0010\u000b\u001a\u0004\u0018\u00010\f2\u0006\u0010\r\u001a\u00020\u000e2\b\u0010\u000f\u001a\u0004\u0018\u00010\u00102\b\u0010\u0011\u001a\u0004\u0018\u00010\u0012H\u0016J\b\u0010\u0013\u001a\u00020\tH\u0002J\b\u0010\u0014\u001a\u00020\tH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0004\n\u0002\u0010\u0007\u00a8\u0006\u0015"}, d2 = {"Lcom/example/android/guesstheword/screens/game/GameFragment;", "Landroidx/fragment/app/Fragment;", "()V", "binding", "Lcom/example/android/guesstheword/databinding/GameFragmentBinding;", "viewModel", "error/NonExistentClass", "Lerror/NonExistentClass;", "gameFinished", "", "onCorrect", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onEndGame", "onSkip", "app_debug"})
+public final class GameFragment extends androidx.fragment.app.Fragment {
+    private com.example.android.guesstheword.databinding.GameFragmentBinding binding;
+    private error.NonExistentClass viewModel;
+    private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    /**
+     * Methods for button click handlers
+     */
+    private final void onSkip() {
+    }
+    
+    private final void onCorrect() {
+    }
+    
+    /**
+     * Called when the game is finished
+     */
+    private final void gameFinished() {
+    }
+    
+    private final void onEndGame() {
+    }
+    
+    public GameFragment() {
+        super();
+    }
+}
